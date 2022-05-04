453535c7770ead192db659349fefd3dabd2e86ca
ndcontrerasr <ndcontrerasr@unal.edu.co>
2022-05-03 20:52:23 -0500

    desarrollo del micro servicio con la creacion de llaves

--- src/routes/llaves.js
+++ src/routes/llaves.js
@@ -4,37 +4,52 @@ const _ = require('underscore');
 
 const llaves = require('../ejemplo.json');
 
+const NodeRSA = require('node-rsa');
+
+
+function armar(datos){
+	var mat = datos[1];
+	for(var i = 2; i < datos.length && i != datos.length-1; i++){
+		mat = mat + datos[i];
+	}
+	return mat
+}
+
 // routes - rutas
 router.get('/key', (req,res) => {
 	res.json(llaves);
-	// res.send({"Titulo":"Hola mundo"});
 });
 
-router.post('/newkey', (req, res) => {
-	// console.log(req.body);
-	var { id, public, private } = req.body;
-	console.log(typeof(id), " | ", typeof(public), " | ", typeof(private));
-
-	if (id && public && private &&  (typeof(id)=="number" && typeof(public)=="number" && typeof(private)=="number")){
-		const newkey = {...req.body};
+router.post('/newkey/:id', (req, res) => {
+	const key = new NodeRSA({b: 128});
+	var { id } = req.params;
+	var public = armar(key.exportKey('public').split('\n'));
+	var private = armar(key.exportKey('private').split('\n'));
+	
+	// var { id, public, private } = req.body;
+	// console.log(typeof(id), " | ", typeof(public), " | ", typeof(private));
+	// console.log(id, "\n", public, "\n", private);
+	
+	if (id && !isNaN(id)){
+		const newkey = {"id": parseInt(id), "public": public, "private": private};
+		// console.log(newkey);
 		llaves.push(newkey);
 		res.json(newkey);
-
 	} else {
 		res.status(500).json({error: "There was an error."});
 	}
-	// res.send('Wrong Request');
 });
 
 router.put('/update/:id', (req, res) => {
+
+	const key = new NodeRSA({b: 128});
 	var { id } = req.params;
-	const { public, private } = req.body;
-	console.log(typeof(id));
-	console.log(public, " | ", private);
-	if (id && public && private &&  (!isNaN(id) && typeof(public)=="number" && typeof(private)=="number")){
-		id = parseInt(id);
+	var public = armar(key.exportKey('public').split('\n'));
+	var private = armar(key.exportKey('private').split('\n'));
+
+	if (id && !isNaN(id)){
 		_.each(llaves, (llave, i) => {
-			if (llave.id == id) {
+			if (llave.id == parseInt(id)) {
 				llave.public = public;
 				llave.private = private;
 			}
@@ -47,15 +62,13 @@ router.put('/update/:id', (req, res) => {
 });
 
 router.delete(`/delete/:id`, (req, res) => {
-
 	const { id } = req.params;
 	_.each(llaves, (llave, i) => {
-		if(llave.id == id) {
+		if(llave.id == parseInt(id)) {
 			llaves.splice(i,1);
 		}
 	});
 	res.send(llaves);
-
 });
 
 module.exports = router;
