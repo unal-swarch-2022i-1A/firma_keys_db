fc2b25493d1d56380aad63ba19ebc1d8f5e7bc57
FredyRosero <faroseroc@unal.edu.co>
2022-06-06 01:13:16 -0500

    Testing requiring a exported function (fibonacci example)

--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,15 @@ function foo(arg) {
     return arg + ".bar"
 }
 
+function fibonacci(n) {
+    if (n === 0 || n === 1)
+        return n;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
 module.exports = {
-    foo: foo
+    foo: foo,
+    fibonacci: fibonacci
 }
